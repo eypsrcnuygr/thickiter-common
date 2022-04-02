6797df8e25f6c4581469e7cf1d436894726ee978
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-04-02 16:22:21 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Re-export stuff from errors and middlewares
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-errors";
@@ -9,13 +10,12 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handlers";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
+
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-created-event";
 export * from "./events/ticket-updated-event";
-
 export * from "./events/types/order-status";
-
-export * from "./events/order-created";
 export * from "./events/order-cancelled";
+export * from "./events/order-created";
